0ccef2be0b57c98496696c96e3163a359bc0623e
andre groseth <andregroseth@hotmail.com>
2020-01-08 09:03:25 +0100

    Starting Implementation of the Burger Builder Container

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -86,8 +86,6 @@ module.exports = function(webpackEnv) {
         loader: MiniCssExtractPlugin.loader,
         options: shouldUseRelativeAssetPaths ? { publicPath: '../../' } : {},
           importLoaders: 1,
-          modules: true,
-          localIdentName: '[name]__[local]__[hash:base64:5]'
       },
       {
         loader: require.resolve('css-loader'),
